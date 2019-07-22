6b12d23e8c3e68548ba1561967acd181c0547317
Petar Peshev <petar.peshev@leanplum.com>
2019-07-22 22:49:18 +0300

    change sh step

--- deploymentPipeline.groovy
+++ deploymentPipeline.groovy
@@ -38,6 +38,7 @@ pipeline {
             dir("target")       
             CRUMB = sh (script: """curl -s 'http://admin:d37b341d117b4e1c9968087a0931650b@${params.JENKINS_URL}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'""",returnStdout: true)      
             echo "$CRUMB"
+            sh 'pwd && ls .'
             sh """curl -X POST -H "'$CRUMB'" --user admin:d37b341d117b4e1c9968087a0931650b -i -F file=@ghprb.hpi http://${params.JENKINS_URL}/pluginManager/uploadPlugin"""
         }
       }
