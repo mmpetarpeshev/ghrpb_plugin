998506eaaa7bfc62e5f7bac552cc3f8d13724b3a
Petar Peshev <petar.peshev@leanplum.com>
2019-07-22 18:34:19 +0300

    change sh step

--- deploymentPipeline.groovy
+++ deploymentPipeline.groovy
@@ -23,27 +23,23 @@ pipeline {
 
   stages {
     
-  /*stage("Build ghrp plugin from leanplum fork repository...") {
+  stage("Build ghrp plugin from leanplum fork repository...") {
       steps {
         script {
             sh '${MAVEN_HOME}/bin/mvn package' 
         }
       }
-    }*/
+    }
 
     stage ("Install the plugin to selected jenkins instance...") {
       steps {
         script {
           echo "Installing plugin ...."
-          //String CRUMB_URL = """http://admin:1178ebd333f5adbab57b3e98f42a673177@${params.JENKINS_URL}/crumbIssuer/api/xml?xpath=concat\\(//crumbRequestField,":",//crumb\\)"""
-          //echo "$CRUMB_URL"
-          try {
-            
+            dir("target")       
             CRUMB = sh (script: """curl -s 'http://admin:d37b341d117b4e1c9968087a0931650b@${params.JENKINS_URL}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'""",returnStdout: true)      
             echo "$CRUMB"
-            sh """curl -X POST -H "'$CRUMB'" --user admin:d37b341d117b4e1c9968087a0931650b -i -F file=@ghprb.hpi ${params.JENKINS_URL}/pluginManager/uploadPlugin"""
-          } catch (Exception e) {
-             echo e.getMessage()
+            sh """curl -X POST -H "'$CRUMB'" --user admin:d37b341d117b4e1c9968087a0931650b -i -F file=@ghprb.hpi http://${params.JENKINS_URL}/pluginManager/uploadPlugin"""
+          }
           }
         }
       }
