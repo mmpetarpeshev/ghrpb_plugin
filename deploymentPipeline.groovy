deac5ad554f804452dc71ce54e38111eeac5ad5e
Petar Peshev <petar.peshev@leanplum.com>
2019-07-22 17:15:13 +0300

    change sh step

--- deploymentPipeline.groovy
+++ deploymentPipeline.groovy
@@ -40,7 +40,7 @@ pipeline {
           try {
             
             //CRUMB = sh (script: """curl -s 'http://admin:d37b341d117b4e1c9968087a0931650b@${params.JENKINS_URL}/crumbIssuer/api/xml?xpath=concat\\(//crumbRequestField,":",//crumb\\)'""",returnStdout: true)      
-            sh "export CRUMB=$\\(curl -s 'http://admin:d37b341d117b4e1c9968087a0931650b@${params.JENKINS_URL}/crumbIssuer/api/xml?xpath=concat\\(//crumbRequestField,":",//crumb\\)'\\) && echo $CRUMB"
+            sh "export CRUMB=\$\\(curl -s 'http://admin:d37b341d117b4e1c9968087a0931650b@${params.JENKINS_URL}/crumbIssuer/api/xml?xpath=concat\\(//crumbRequestField,":",//crumb\\)'\\) && echo $CRUMB"
             sh """curl -X POST -H "'$CRUMB'" --user admin:d37b341d117b4e1c9968087a0931650b -i -F file=@ghprb.hpi ${params.JENKINS_URL}/pluginManager/uploadPlugin"""
           } catch (Exception e) {
              echo e.getMessage()
