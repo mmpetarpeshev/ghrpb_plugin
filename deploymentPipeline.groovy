e17c1234042fffee8940089a4bcbb3d31d6c9b24
Petar Peshev <petar.peshev@leanplum.com>
2019-07-23 14:04:08 +0300

    test the install

--- deploymentPipeline.groovy
+++ deploymentPipeline.groovy
@@ -39,7 +39,7 @@ pipeline {
             def CRUMB = sh (script: """curl -s 'http://admin:d37b341d117b4e1c9968087a0931650b@${params.JENKINS_URL}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'""",returnStdout: true)      
             //def FCRUMB = sh (script: "$CRUMB | cut -d ':' -f2")
             echo "$CRUMB"         
-            sh """curl -X POST -H "'$CRUMB'" --user admin:d37b341d117b4e1c9968087a0931650b -i -F file=@ghprb.hpi http://${params.JENKINS_URL}/pluginManager/uploadPlugin"""
+            sh """curl -X POST -H "$CRUMB" --user admin:d37b341d117b4e1c9968087a0931650b -i -F file=@ghprb.hpi http://${params.JENKINS_URL}/pluginManager/uploadPlugin"""
         }
        }
       }
